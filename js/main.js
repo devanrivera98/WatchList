afa64556bb5d8b92c62ab9ca9e66e092b4cd2490
Devan Rivera <devanrivera98@gmail.com>
2023-03-01 18:21:33 +0000

    rendered a DOM tree for every movie that matches the name in the functions parameters

--- js/main.js
+++ js/main.js
@@ -3,25 +3,11 @@
 // var $searchButton2 = document.querySelector('#search-button');
 // var $main = document.querySelector('[data-view="homepage"]');
 // var $section = document.querySelector('[data-view="results"');
-// var $ul = document.querySelector('ul');
+var $ul = document.querySelector('ul');
 // console.log($searchForm);
 // console.log($searchButton1);
 // console.log($searchButton2);
 // console.log($section);
-
-// function renderMovie(object) {
-//   var $resultsBackground = document.createElement('div');
-//   $resultsBackground.className = 'results-background';
-//   var $movieBackground = document.createElement('div');
-//   $movieBackground.className = 'movie-background';
-//   var $movieTitle = document.createElement('h2');
-//   $movieTitle.textContent = 'placeholder';
-//   var $moviePoster = document.createElement('img');
-//   $moviePoster.setAttribute('src', 'image link placeholder');
-//   var $learnMore = document.createElement('a');
-//   $learnMore.textContent = 'LEARN MORE';
-//   // $learnMore WILL NEED TO BE CHANGED TO AN ELEMENT WILL HAVE TO BE CREATED HERE THAT WILL LINK TO THE FOLLOWING MORE INFO PAGE
-// }
 
 // function ViewSwap(name) {
 //   if (name === 'homepage') {
@@ -33,3 +19,38 @@
 //   }
 //   data.view = name;
 // }
+
+function getMovieName(name) {
+  var xhr = new XMLHttpRequest();
+  // console.log('https://imdb-api.com/API/AdvancedSearch/k_99uf6ywj/?title=' + name);
+  xhr.open('GET', 'https://imdb-api.com/API/AdvancedSearch/k_99uf6ywj/?title=' + name);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    // console.log('xhr.status', xhr.status);
+    // console.log('xhr.response', xhr.response);
+    for (var i = 0; i < 5; i++) {
+      var $li = document.createElement('li');
+      var $resultsBackground = document.createElement('div');
+      $resultsBackground.className = 'results-background';
+      var $movieBackground = document.createElement('div');
+      $movieBackground.className = 'results-second-background';
+      var $movieTitle = document.createElement('h2');
+      $movieTitle.setAttribute('id', 'title');
+      $movieTitle.textContent = xhr.response.results[i].title;
+      var $moviePoster = document.createElement('img');
+      $moviePoster.setAttribute('src', xhr.response.results[i].image);
+      var $learnMore = document.createElement('a');
+      $learnMore.setAttribute('id', 'learn-more');
+      $learnMore.textContent = 'Learn More';
+      $li.appendChild($resultsBackground);
+      $resultsBackground.appendChild($movieBackground);
+      $movieBackground.appendChild($movieTitle);
+      $movieBackground.appendChild($moviePoster);
+      $movieBackground.appendChild($learnMore);
+      $ul.appendChild($li);
+    }
+  });
+  xhr.send();
+}
+
+getMovieName(); // no purpose just so I can commit
