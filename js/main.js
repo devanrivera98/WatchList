311e257aa44ddcb190526e0b3e8d3976c64c722d
Devan Rivera <devanrivera98@gmail.com>
2023-03-01 06:21:17 +0000

    Create a mobile results page

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// var $searchForm = document.querySelector('.search');
+// var $searchButton1 = document.querySelector('i');
+// var $searchButton2 = document.querySelector('#search-button');
+// var $main = document.querySelector('[data-view="homepage"]');
+// var $section = document.querySelector('[data-view="results"');
+// var $ul = document.querySelector('ul');
+// console.log($searchForm);
+// console.log($searchButton1);
+// console.log($searchButton2);
+// console.log($section);
+
+// function renderMovie(object) {
+//   var $resultsBackground = document.createElement('div');
+//   $resultsBackground.className = 'results-background';
+//   var $movieBackground = document.createElement('div');
+//   $movieBackground.className = 'movie-background';
+//   var $movieTitle = document.createElement('h2');
+//   $movieTitle.textContent = 'placeholder';
+//   var $moviePoster = document.createElement('img');
+//   $moviePoster.setAttribute('src', 'image link placeholder');
+//   var $learnMore = document.createElement('a');
+//   $learnMore.textContent = 'LEARN MORE';
+//   // $learnMore WILL NEED TO BE CHANGED TO AN ELEMENT WILL HAVE TO BE CREATED HERE THAT WILL LINK TO THE FOLLOWING MORE INFO PAGE
+// }
+
+// function ViewSwap(name) {
+//   if (name === 'homepage') {
+//     $main.classList.remove('hidden');
+//     $section.classList.add('hidden');
+//   } else if (name === 'results') {
+//     $main.classList.add('hidden');
+//     $section.classList.remove('hidden');
+//   }
+//   data.view = name;
+// }
