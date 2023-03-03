152f1d3ecdf2e6ef464b11f977bebe0109402faa
Devan Rivera <devanrivera98@gmail.com>
2023-03-03 19:37:42 +0000

    Set up local storage so that when the browser refreshes the movie picked is not deleted.

--- js/data.js
+++ js/data.js
@@ -3,3 +3,15 @@ var data = {
   view: 'homepage',
   entries: []
 };
+
+window.addEventListener('beforeunload', addLocalStorage);
+
+function addLocalStorage(event) {
+  var dataJson = JSON.stringify(data);
+  var dataLocalStorage = localStorage.setItem('javascript-local-storage', dataJson);
+  return dataLocalStorage;
+}
+
+if (localStorage.getItem('javascript-local-storage')) {
+  data = JSON.parse(localStorage.getItem('javascript-local-storage'));
+}
